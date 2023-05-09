68c898f83a9a93f6cee58e7dd4eeced9808e5e6c
Excellencior <turjob44@gmail.com>
2023-05-09 23:50:31 +0600

    Double Dice Roll C-Sharp

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+class HelloWorld {
+    static void Main() {
+        int roll1 = -1, roll2 = 0, attempts = 0;
+
+        Random randNumGen = new Random();
+
+        while (roll1 != roll2) {
+            roll1 = randNumGen.Next(1, 7);
+            roll2 = randNumGen.Next(1, 7);
+
+            Console.WriteLine("Roll-1: " + roll1);
+            Console.WriteLine("Roll-2: " + roll2);
+            Console.WriteLine();
+            attempts++;
+        }
+
+        Console.WriteLine("Total Attempts: " + attempts);
+        Console.ReadKey(); // await for a key press
+    }
+}
+
+
